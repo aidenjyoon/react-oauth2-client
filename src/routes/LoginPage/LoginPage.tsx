ca374fef5de8562515e1552b26282590a4bfaccb
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-05-27 16:15:49 -0700

    update LoginPage.tsx google login button
    - will now redirect to google log in page and redirect back to homepage.

--- src/routes/LoginPage/LoginPage.tsx
+++ src/routes/LoginPage/LoginPage.tsx
@@ -3,12 +3,17 @@ import googleLogo from "../../assets/google_logo.png";
 import styles from "./LoginPage.module.scss";
 
 const LoginPage = () => {
+  // redirects to google sign in with backend
+  const googleLogin = () => {
+    window.open("http://localhost:4000/auth/google", "_self");
+  };
+
   return (
     <>
       <div className={styles.loginPage}>
         <h1>Login</h1>
         <div className={styles.loginForm}>
-          <div className={styles.googleContainer}>
+          <div className={styles.googleContainer} onClick={googleLogin}>
             <img src={googleLogo} alt="Google Logo" />
             <p>Login with Google</p>
           </div>
